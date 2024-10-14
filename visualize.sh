b71bcc8ab13db889eb9e35c3a0773d1f17ed767a
Jarin <1121278235@qq.com>
2024-10-14 15:05:32 +0800

    save

--- visualize.sh
+++
@@ -1,1 +0,0 @@
-python visualize_json_results.py --input output/mechmind/mechmind_10_15_language_adapter/coco_instances_results.json --output output/mechmind/mechmind_10_15_language_adapter/visualization --dataset mechmind_hard --conf-threshold 0.3
